680ef48e544c9755d32ca60baa8fc79af240b13a
surendra172001 <sap172001@gmail.com>
2020-09-20 23:12:54 +0530

    This will start image gallery project

---
+++ image-gallery/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Gallery from './components/Gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery/>
+    </div>
+  );
+}
+
+export default App;
---
+++ image-gallery/src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function Gallery() {
+    return (
+        <div className="main-container">
+            <p>-----Image Gallery-----</p>
+            <input />
+            <button>Add image</button>
+        </div>
+    );
+}
+
+export default Gallery;
